b9415a5d009308141f2956eed838f30a6a499958
Alex Young <alexyoung0119@gmail.com>
2025-04-22 14:53:08 -0500

    quick mobile layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,13 @@ export default function Home() {
   return (
     <main className="h-screen md:py-14 md:px-11 px-7 py-10 overflow-hidden bg-background transition-colors duration-1000">
       <div id="border" className="border-[.05px] duration-1000 transition-colors bg-background border-foreground h-full grid lg:grid-cols-2 place-items-center">
-        <span id="name" className="flex flex-col gap-4 lg:mt-0 mt-16">
-          <h1 className="text-6xl font-extralight tracking-wide">Alex Young</h1>
-          <RotatingText messages={titles} textClassName="text-2xl font-extralight" />
+        <span id="name" className="flex flex-col gap-4 lg:mt-0 mt-12">
+          <h1 className="lg:text-6xl text-4xl font-extralight tracking-wide">Alex Young</h1>
+          <RotatingText messages={titles} textClassName="lg:text-2xl text-xl font-extralight" />
         </span>
         <div id="scroll tabs">
           <div className="p-8 no-scrollbar h-full flex flex-col gap-8">
-            <InfiniteScrollNav items={sections} className="lg:h-[32rem] h-[40rem] w-96 scrollbar-hide border-none" containerClassName="space-y-16 lg:text-4xl text-4xl tracking-wide" />
+            <InfiniteScrollNav items={sections} className="lg:h-[32rem] h-[24rem] lg:w-96 scrollbar-hide border-none" containerClassName="space-y-16 lg:text-4xl text-2xl tracking-wide" />
           </div>
         </div>
       </div>
